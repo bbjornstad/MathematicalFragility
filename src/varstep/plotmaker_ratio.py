754c53f903c7281680610222327f7265dab3fc4c
Bailey Bjornstad <baileybjornstad@gmail.com>
2017-09-04 13:41:32 -0500

    Fixed ratio plot bounds

--- src/varstep/plotmaker_ratio.py
+++ src/varstep/plotmaker_ratio.py
@@ -1,7 +1,7 @@
 from numpy import arange, load
 import matplotlib.pyplot as plt
 
-init_pos = 1000
+init_pos = 100
 # set some indices
 means = load('data/means_from_{0}.npy'.format(init_pos))
 stds = load('data/stds_from_{0}.npy'.format(init_pos))
@@ -35,6 +35,7 @@ for splot in m_splots:
     splot.set_xlabel('Step size', fontsize=label_fontsize)
     splot.set_ylabel('Mean hitting time', fontsize=label_fontsize)
     splot.tick_params(axis='both', labelsize=axis_fontsize)
+    splot.set_ybound(0,500)
 m_splots[0].set_title(r'$\beta < 1$', fontsize=label_fontsize)
 m_splots[1].set_title(r'$\beta \geq 1$', fontsize=label_fontsize)
 
@@ -57,6 +58,7 @@ for splot in s_splots:
     splot.set_xlabel('Step size', fontsize=label_fontsize)
     splot.set_ylabel('STD of hitting time', fontsize=label_fontsize)
     splot.tick_params(axis='both', labelsize=axis_fontsize)
+    splot.set_ybound(0,500)
 s_splots[0].set_title(r'$\beta < 1$', fontsize=label_fontsize)
 s_splots[1].set_title(r'$\beta \geq 1$', fontsize=label_fontsize)
 
